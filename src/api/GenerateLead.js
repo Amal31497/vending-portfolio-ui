95796fde17965617e213fd4bd4fca3671d60d350
Amal Janabayev <amalj2426@gmail.com>
2023-01-24 15:45:53 -0800

    Updated api andpoint

--- src/api/GenerateLead.js
+++ src/api/GenerateLead.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = `https://vendingportfolio-env.eba-7zbsnfmb.us-west-2.elasticbeanstalk.com:8080/api/lead`;
+const baseUrl = `https://vendingportfolio-env.eba-7zbsnfmb.us-west-2.elasticbeanstalk.com/api/lead`;
 
 const GenerateLead = {
     createLead: (newLead) => {
