30c91b497869b8c37025c3385e71fc33c594bb29
Amal Janabayev <amalj2426@gmail.com>
2023-02-19 22:55:48 -0800

    Added referral link

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,9 +7,21 @@ function Contact(){
 
     return (
         <div className="contactPageWrapper">
-            <div style={{width:"100%", marginTop:"50px"}}>
-                <p style={{marginBottom:"50px", fontWeight:"800"}}>Get in touch with us to learn more about our services</p>
-                <ContactForm />
+            <div className="row" style={{ width: "90%", display:"flex", alignContent:"center", height:"100%"}}>
+                <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <h4 style={{marginTop:"5%",marginBottom:"2%", fontWeight:"900"}}>Get in touch with us to learn more about our services</h4>
+                    <ContactForm />
+                </div>
+                <div  className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <h4 style={{ marginTop:"5%",marginBottom:"2%", fontWeight:"900" }}>Our Referral Program</h4>
+                    <p style={{ marginTop:"5%",marginBottom:"10%", fontWeight:"900"}}>
+                        Does your establishment need a vending machine? 
+                        If you connect us with your building's management 
+                        and it results in the vending machine placement, 
+                        you may receive upto 300$ as a finder's fee from 
+                        Snackster LLC
+                    </p>
+                </div>
             </div>
             <Footer />
         </div>
